e4b2cb08a7b5b35aa64cbdcd42d407d23a64a1d8
alexrauscher <alexanderrauscher@gmx.at>
2015-12-11 13:59:24 +0100

    GUI subject detail anpassung

--- subject_detail.cpp
+++ subject_detail.cpp
@@ -1,26 +1,14 @@
 #include "subject_detail.h"
 #include "ui_subject_detail.h"
 
-subject_detail::subject_detail(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::subject_detail)
+Subject_detail::Subject_detail(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Subject_detail)
 {
     ui->setupUi(this);
-    connect(pushButton_2, SIGNAL(click()), this, SLOT(openNote()));
-    connect(pushButton_3, SIGNAL(click()), this, SLOT(openNote()));
-    connect(pushButton_4, SIGNAL(click()), this, SLOT(openNote()));
-    connect(pushButton_5, SIGNAL(click()), this, SLOT(openNote()));
-    connect(pushButton_6, SIGNAL(click()), this, SLOT(openNote()));
-    connect(pushButton_8, SIGNAL(click()), this, SLOT(openNote()));
 }
 
-subject_detail::~subject_detail()
+Subject_detail::~Subject_detail()
 {
     delete ui;
-}
-
-void subject_detail::openNote()
-{
-   newWindow = new MRichTextEdit(this); // Be sure to destroy you window somewhere
-   newWindow->show();
 }
--- subject_detail.h
+++ subject_detail.h
@@ -1,35 +1,22 @@
 #ifndef SUBJECT_DETAIL_H
 #define SUBJECT_DETAIL_H
 
-#include <QMainWindow>
-#include <QPushButton>
-#include "mrichtextedit.h"
-#include "ui_subject_detail.h"
+#include <QWidget>
 
 namespace Ui {
-class subject_detail;
+class Subject_detail;
 }
 
-class subject_detail : public QMainWindow
+class Subject_detail : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit subject_detail(QWidget *parent = 0);
-    ~subject_detail();
-
-public slots:
-   void openNote();
+    explicit Subject_detail(QWidget *parent = 0);
+    ~Subject_detail();
 
 private:
-    Ui::subject_detail *ui;
-    MRichTextEdit *newWindow;
-    QPushButton *pushButton_2;
-    QPushButton *pushButton_3;
-    QPushButton *pushButton_4;
-    QPushButton *pushButton_5;
-    QPushButton *pushButton_6;
-    QPushButton *pushButton_8;
+    Ui::Subject_detail *ui;
 };
 
 #endif // SUBJECT_DETAIL_H
