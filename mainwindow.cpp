353d4361fc2138b6b86f2763c8f6576983d74d03
Philipp Korkodilos <phil.korkodilos@gmail.com>
2016-02-11 22:52:45 +0100

    Added Automatic Refresh

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,7 @@
 #include <exportdialog.h>
 #include <dbhandler.h>
 #include <QGroupBox>
+#include <main.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,6 +18,13 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::update(Subject creSub)
+{
+//this->sublist = pDBh->queryWithReturnSubjectList("SELECT * FROM schoolsubject");
+qDebug() << "New Subject: " << sublist[sublist.length()-1].getName();
+qDebug() << "Real New Subject: " << creSub.getName();
 }
 
 void MainWindow::setSubList(QList<Subject> list)
@@ -77,7 +85,7 @@ void MainWindow::setSubList(QList<Subject> list)
 
 void MainWindow::openNewWindow()
 {
-    newCre = new c_subject(); // Be sure to destroy you window somewhere
+    newCre = new c_subject(this); // Be sure to destroy you window somewhere
     newCre->show();
 }
 
